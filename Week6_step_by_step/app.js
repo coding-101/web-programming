36fcdf32b50bceb5b8e98c84623ee4951e055a27
Ju-hyeoung, Park <dkdkajej@gmail.com>
2017-03-25 02:59:14 +0900

    add model and sample data

--- Week6_step_by_step/app.js
+++ Week6_step_by_step/app.js
@@ -12,6 +12,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+var TodoList = require('./models/todolist');
+
+var default_todo_lists = [
+  {
+    title: 'Hello Todo List!',
+    current_todos: [
+      {
+        title: '새로운 Todo List 생성하기',
+        description: '오른쪽 하단의 버튼 위에 마우스를 올려 빨간색 작은 아이콘을 클릭하여 새로운 Todo List를 생성 해보세요!'
+      }
+    ],
+    completed_todos: []
+  }
+]
+
 app.get('/', function (req, res) {
   res.render('index');
 })
